0291831e029ff7056f43e6c96c184b56b2ac9d4a
apobekiaris <apostolis.bekiaris@gmail.com>
2018-11-30 08:13:23 +0200

    Support for WinFormsTheoryAttribute #17
    # Conflicts:
    #	src/Xunit.StaFact/WpfTheoryAttribute.cs
    #	src/Xunit.StaFact/Xunit.StaFact.csproj

---
+++ src/Xunit.StaFact.Tests/desktop/WinFormsTheoryTests.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Ms-PL license. See LICENSE.txt file in the project root for full license information.
+
+#if !NET45
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using Xunit;
+
+public class WinFormsTheoryTests
+{
+    [WinFormsTheory]
+    [InlineData(0)]
+    [InlineData(1)]
+    public async Task WinFormsTheory_OnSTAThread(int arg)
+    {
+        Assert.IsType<WindowsFormsSynchronizationContext>(SynchronizationContext.Current);
+        Assert.Equal(ApartmentState.STA, Thread.CurrentThread.GetApartmentState());
+        await Task.Yield();
+        Assert.Equal(ApartmentState.STA, Thread.CurrentThread.GetApartmentState()); // still there
+        Assert.IsType<WindowsFormsSynchronizationContext>(SynchronizationContext.Current);
+        Assert.True(arg == 0 || arg == 1);
+    }
+
+    [Trait("Category", "FailureExpected")]
+    [WinFormsTheory]
+    [InlineData(0)]
+    [InlineData(1)]
+    public async Task WinFormsTheoryFails(int arg)
+    {
+        Assert.IsType<WindowsFormsSynchronizationContext>(SynchronizationContext.Current);
+        Assert.Equal(ApartmentState.STA, Thread.CurrentThread.GetApartmentState());
+        await Task.Yield();
+        Assert.Equal(ApartmentState.STA, Thread.CurrentThread.GetApartmentState()); // still there
+        Assert.IsType<WindowsFormsSynchronizationContext>(SynchronizationContext.Current);
+        Assert.True(arg == 0 || arg == 1);
+    }
+}
+
+#endif
