5a0b21869cc4cacd80826d29beb3c3d2e790d316
Andrew Arnott <andrewarnott@gmail.com>
2020-05-16 15:34:02 -0600

    Sleep the WinForms thread without spinning
    Fixes #43

--- src/Xunit.StaFact/Sdk.Desktop/WinFormsSynchronizationContextAdapter.cs
+++ src/Xunit.StaFact/Sdk.Desktop/WinFormsSynchronizationContextAdapter.cs
@@ -4,6 +4,8 @@
 namespace Xunit.Sdk
 {
     using System;
+    using System.ComponentModel;
+    using System.Runtime.InteropServices;
     using System.Threading;
     using System.Threading.Tasks;
     using System.Windows.Forms;
@@ -11,6 +13,12 @@ namespace Xunit.Sdk
     internal class WinFormsSynchronizationContextAdapter : SyncContextAdapter
     {
         internal static readonly SyncContextAdapter Default = new WinFormsSynchronizationContextAdapter();
+
+#pragma warning disable SA1310 // Field names should not contain underscore
+        private const uint MWMO_INPUTAVAILABLE = 0x0004;
+        private const uint QS_ALLINPUT = 0x04FF;
+        private const uint WAIT_FAILED = 0xFFFFFFFF;
+#pragma warning restore SA1310 // Field names should not contain underscore
 
         private WinFormsSynchronizationContextAdapter()
         {
@@ -30,10 +38,21 @@ namespace Xunit.Sdk
             while (!task.IsCompleted)
             {
                 Application.DoEvents();
-                Thread.Sleep(0); // give up thread to OS so we don't spin CPU too hard
+                if (MsgWaitForMultipleObjectsEx(0, IntPtr.Zero, 250, QS_ALLINPUT, MWMO_INPUTAVAILABLE) == WAIT_FAILED)
+                {
+                    throw new Win32Exception();
+                }
             }
         }
 
         internal override void InitializeThread() => Application.OleRequired();
+
+        [DllImport("user32", SetLastError = true)]
+        private static extern uint MsgWaitForMultipleObjectsEx(
+            uint nCount,
+            IntPtr pHandles,
+            uint dwMilliseconds,
+            uint dwWakeMask,
+            uint dwFlags);
     }
 }
