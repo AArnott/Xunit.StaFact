9e0e99fecef67af4e0d151dea2a4daba7b90579d
Andrew Arnott <andrewarnott@live.com>
2021-05-15 14:52:55 -0600

    Fix build warnings

--- src/Xunit.StaFact.Tests/ParallelExecutionLimit/UITest06.cs
+++ src/Xunit.StaFact.Tests/ParallelExecutionLimit/UITest06.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Ms-PL license. See LICENSE.txt file in the project root for full license information.
+
 using Xunit;
 
 public class UITest06 : UITestBase
