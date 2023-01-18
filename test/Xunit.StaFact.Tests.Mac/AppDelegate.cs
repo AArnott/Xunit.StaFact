0bdc56751e9b364517165b732779e4d9d1ee8d97
Sandy Armstrong <sandy@xamarin.com>
2023-01-18 11:50:22 -0800

    Use Mac app as harness for xunit test runner
    Need a full app bundle in order to start Cocoa mainloop.

---
+++ test/Xunit.StaFact.Tests.Mac/AppDelegate.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Ms-PL license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.InteropServices;
+
+namespace Xunit.StaFact.Tests.Mac;
+
+[Register("AppDelegate")]
+public partial class AppDelegate : NSApplicationDelegate
+{
+    public override void DidFinishLaunching(NSNotification notification)
+        => ThreadPool.QueueUserWorkItem(
+            o => _exit(
+                Xunit.ConsoleClient.Program.Main(new[]
+                {
+                    typeof(AppDelegate).Assembly.Location,
+                    "-appdomains",
+                    "denied",
+                })));
+
+    [LibraryImport(ObjCRuntime.Constants.libcLibrary)]
+#pragma warning disable SA1300 // Element should begin with upper-case letter
+    private static partial void _exit(int exitCode);
+#pragma warning restore SA1300 // Element should begin with upper-case letter
+}
--- test/Xunit.StaFact.Tests.Mac/CocoaFactTests.cs
+++ test/Xunit.StaFact.Tests.Mac/CocoaFactTests.cs
@@ -1,32 +1,30 @@
-﻿using System.Threading.Tasks;
-using AppKit;
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Ms-PL license. See LICENSE file in the project root for full license information.
+
 using Xunit;
+
 public class CocoaFactTests
 {
     [CocoaFact]
     public void AssertMainThreadSync()
         => NSApplication.EnsureUIThread();
+
     [Fact]
     public void AssertMainThreadSyncAndFail()
         => Assert.Throws<AppKitThreadAccessException>(NSApplication.EnsureUIThread);
+
     [CocoaFact]
     public Task AssertMainThreadAsyncReturnCompletedTask()
     {
         NSApplication.EnsureUIThread();
         return Task.CompletedTask;
     }
+
     [CocoaFact]
     public async Task AssertMainThreadAsyncWithAwait()
     {
         await Task.Yield();
         NSApplication.EnsureUIThread();
         await Task.Delay(100);
-    }
-
-    [CocoaFact]
-    public void AssertExceptionsAreCaptured()
-    {
-        NSApplication.EnsureUIThread();
-        throw new System.Exception("Thrown from Main");
     }
 }
