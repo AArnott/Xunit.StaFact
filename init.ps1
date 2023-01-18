b13aa1d2828bfcf86c81ce9495205147e7c35fed
Sandy Armstrong <sandy@xamarin.com>
2023-01-18 15:20:50 -0800

    Basic workload-from-rollback provisioning
    This allows building on macOS 12 and easily consuming the nupkg from
    projects still using older workload versions.

--- init.ps1
+++ init.ps1
@@ -66,7 +66,18 @@ if (!$NoPrerequisites) {
         $EnvVars['PROCDUMP_PATH'] = & "$PSScriptRoot\azure-pipelines\Get-ProcDump.ps1"
     }
 
-    dotnet workload install macos
+    # Install specific macOS workload version in order to support usage on older .NET and macOS
+    $rollbackFile = Join-Path ([System.IO.Path]::GetTempPath()) ([System.Guid]::NewGuid())
+    @{
+        "microsoft.net.sdk.macos" = "12.3.2372/7.0.100"
+    } | ConvertTo-Json | Set-Content -Path $rollbackFile
+
+    try {
+        dotnet workload install macos --from-rollback-file $rollbackFile
+    }
+    finally {
+        Remove-Item $rollbackFile
+    }
 }
 
 # Workaround nuget credential provider bug that causes very unreliable package restores on Azure Pipelines
