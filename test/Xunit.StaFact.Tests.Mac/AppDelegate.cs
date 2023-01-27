67c4d9214626a8b78a75f4362443521659a7ca7b
Sandy Armstrong <sandy@xamarin.com>
2023-01-27 10:49:23 -0800

    XML test results generating locally

--- test/Xunit.StaFact.Tests.Mac/AppDelegate.cs
+++ test/Xunit.StaFact.Tests.Mac/AppDelegate.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the Ms-PL license. See LICENSE file in the project root for full license information.
 
+
 using System.Runtime.InteropServices;
 
 namespace Xunit.StaFact.Tests.Mac;
@@ -15,21 +16,14 @@ public class AppDelegate : NSApplicationDelegate
         var args = NSProcessInfo.ProcessInfo.Arguments;
         for (int i = 1; i < args.Length; i++)
         {
-            var arg = args[i];
-            if(arg == "xunit")
-                continue;
-
-            unitTestDriverArguments.Add(arg);
+            unitTestDriverArguments.Add(args[i]);
         }
     }
     public override void DidFinishLaunching(NSNotification notification)
     {
-        // if we've been given at least one non-help argument, append
-        // disable AppDomain support since we need all tests to run
+        // Disable AppDomain support since we need all tests to run
         // in the main app domain to be able to run on or dispatch to
         // the main thread.
-        var thisAssemblyLocation = typeof(AppDelegate).Assembly.Location;
-        unitTestDriverArguments.Add(thisAssemblyLocation);
         unitTestDriverArguments.Add("-appdomains");
         unitTestDriverArguments.Add("denied");
 
