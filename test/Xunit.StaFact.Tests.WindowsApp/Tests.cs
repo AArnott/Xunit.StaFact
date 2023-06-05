4af07b6717424ca88623cdc8e51e0edf74a1ced8
Andrew Arnott <andrew.arnott@microsoft.com>
2023-06-05 14:12:38 -0600

    Add a test app project as a sandbox to try to get any WinUI tests to run

---
+++ test/Xunit.StaFact.Tests.WindowsApp/Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using Xunit.StaFact.Tests.WindowsApp;
+
+[TestClass]
+public class Tests
+{
+    //[WinUIFact]
+    [UITestMethod]
+    public void Hi()
+    {
+        //var window = new MainWindow();
+        //window.Activate();
+
+        //Assert.True(window.Visible);
+    }
+}
